b54d207291a1194ce8b67d1d5fb85c0e970d7665
letsleepingfoxeslie <238576142+letsleepingfoxeslie@users.noreply.github.com>
2026-02-22 04:48:06 -0300

    Feature: added timer, refactored code?

--- 001-timed-math/timed_math.py
+++ 001-timed-math/timed_math.py
@@ -1,17 +1,23 @@
 import random
+import time
 
 # Constants
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND, MAX_OPERAND = 2, 16
 TOTAL_PROBLEMS = 5
 
-def generate_problem() -> str:
+def generate_problem() -> tuple[str, str]:
     left, right = random.randint(MIN_OPERAND, MAX_OPERAND), random.randint(MIN_OPERAND, MAX_OPERAND)
     operator = random.choice(OPERATORS)
 
     expression = f"{left} {operator} {right}"
     answer = eval(expression)
     return expression, answer
+
+
+input("Press ENTER to start!")
+start_time = time.time()
+print("-" * 24)
 
 for problem in range(TOTAL_PROBLEMS):
     expression, answer = generate_problem()
@@ -20,3 +26,7 @@ for problem in range(TOTAL_PROBLEMS):
         if guess == str(answer):
             break
         continue
+
+end_time = time.time()
+print("-" * 24)
+print(f"Time taken: {end_time - start_time:.3f}s")
