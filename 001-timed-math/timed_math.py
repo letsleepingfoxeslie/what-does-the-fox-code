ae584f77eb79b5944b932a46a458200fab7f03b8
letsleepingfoxeslie <238576142+letsleepingfoxeslie@users.noreply.github.com>
2026-02-22 04:42:05 -0300

    Feature: core loop

--- 001-timed-math/timed_math.py
+++ 001-timed-math/timed_math.py
@@ -3,12 +3,20 @@ import random
 # Constants
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND, MAX_OPERAND = 2, 16
+TOTAL_PROBLEMS = 5
 
-def generate_problem():
+def generate_problem() -> str:
     left, right = random.randint(MIN_OPERAND, MAX_OPERAND), random.randint(MIN_OPERAND, MAX_OPERAND)
     operator = random.choice(OPERATORS)
 
     expression = f"{left} {operator} {right}"
-    return expression
+    answer = eval(expression)
+    return expression, answer
 
-print(generate_problem())
+for problem in range(TOTAL_PROBLEMS):
+    expression, answer = generate_problem()
+    while True:
+        guess = input(f"Problem #{problem + 1}: {expression} = ")
+        if guess == str(answer):
+            break
+        continue
