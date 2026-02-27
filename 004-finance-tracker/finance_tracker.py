42e9c88824fcf29f9051807c710784117eb20afe
letsleepingfoxeslie <238576142+letsleepingfoxeslie@users.noreply.github.com>
2026-02-27 04:05:24 -0300

    Feature: finish 004

--- 004-finance-tracker/finance_tracker.py
+++ 004-finance-tracker/finance_tracker.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 from datetime import datetime
 from data_entry import get_amount, get_category, get_date, get_description
-
+import matplotlib.pyplot as plt
 class CSV:
     CSV_FILE: str = r"004-finance-tracker/finance_data.csv"
     COLUMNS: list[str] = ["date", "amount", "category", "description"]
@@ -75,6 +75,23 @@ def prompt_entry() -> None:
 
     CSV.add_entry(date, amount, category, description)
 
+def plot_transactions(df: pd.DataFrame):
+    df.set_index("date", inplace = True)
+
+    # Fills in the missing days and fills them with zero
+    income_df = df[df["category"] == "Income"].resample("D").sum().reindex(df.index, fill_value = 0)
+    expense_df = df[df["category"] == "Expense"].resample("D").sum().reindex(df.index, fill_value = 0)
+
+    plt.figure(figsize = (10, 5))
+    plt.plot(income_df.index, income_df["amount"], label = "Income", color = "g")
+    plt.plot(expense_df.index, expense_df["amount"], label = "Expense", color = "r")
+    plt.xlabel("Date")
+    plt.ylabel("Amount")
+    plt.title("Income and expenses over time")
+    plt.legend() # For labels
+    plt.grid(True) 
+    plt.show()
+
 def main():
     while True:
         print("[1] Add a new transaction")
@@ -89,6 +106,10 @@ def main():
                 start_date: str = get_date("Enter the start date (dd-mm-yyyy): ")
                 end_date: str = get_date("Enter the end date (dd-mm-yyyy) or [ENTER] for today: ", allow_default = True)
                 df: pd.DataFrame = CSV.get_transactions(start_date, end_date)
+
+                if input("Do you want to see a plot? (Y/N) ").lower() == "y":
+                    plot_transactions(df)
+
             case "3":
                 print("Exiting!")
                 exit()
