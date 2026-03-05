fe45e43761ef6af6816a36d9f05ac88bfbaf0d32
letsleepingfoxeslie <238576142+letsleepingfoxeslie@users.noreply.github.com>
2026-03-05 17:33:45 -0300

    Refactor: clean up some code

--- 005-pygame-bepis/bepis.py
+++ 005-pygame-bepis/bepis.py
@@ -1,6 +1,5 @@
 import pygame, sys, random
-from grid import Grid
-from blocks import *
+from game import Game
 
 pygame.init()
 
@@ -8,16 +7,8 @@ pygame.init()
 # (width, height)
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("BEPIS")
-
 clock = pygame.time.Clock()
-game_grid = Grid()
-
-# for i in range(30):
-#    game_grid.GRID[random.randint(0, 19)][random.randint(0, 9)] = random.randint(1, 6)
-# game_grid.print_grid()
-
-block = TBlock()
-
+game = Game()
 
 # Game loop
 while True:
@@ -30,8 +21,7 @@ while True:
 
     # Drawing stuff?
     screen.fill((44, 44, 127))
-    game_grid.draw(screen)
-    block.draw(screen)
+    game.draw(screen)
 
     # Update positions for game objects
     pygame.display.update()
